4b42a537ff1331673eccdff6577c35943fff7b0f
Sana <kara.nemesis@gmail.com>
2025-06-14 19:56:10 +0100

    lab 1

---
+++ tt1/terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.regions
+}
+
+resource "aws_s3_bucket" "task1-bucket" {
+  bucket = "lab_1"
+  aws_s3_bucket_acl    = "private"
+}
